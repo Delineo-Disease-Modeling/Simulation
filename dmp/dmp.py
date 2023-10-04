8ae09f10c68f6a1634637e60ac213f4254386775
Neil Patel <neilpatel369@gmail.com>
2023-10-04 13:40:16 -0400

    Created probability matrix, working on machine

---
+++ dmp/dmp.py
@@ -0,0 +1,34 @@
+import random;
+
+# Define states
+states = ["Symptomatic", "Infectious", "Hospitalized", "ICU", "Removed", "Recovered"]
+
+# Create a transition matrix with transition probabilities
+transition_matrix = [
+    # Symptomatic  Infectious  Hospitalized   ICU      Removed    Recovered
+    [0.0,          0.5,        0.2,          0.0,     0.0,      0.3],   # Symptomatic
+    [0.0,          0.0,        0.4,          0.1,     0.0,      0.5],   # Infectious
+    [0.0,          0.0,        0.0,          0.6,     0.0,      0.4],   # Hospitalized
+    [0.0,          0.0,        0.0,          0.0,     0.3,      0.7],   # ICU
+    [0.0,          0.0,        0.0,          0.0,     1.0,      0.0],   # Removed
+    [0.0,          0.0,        0.0,          0.0,     0.0,      1.0]    # Recovered
+]
+
+# Initialize the initial state (e.g., a person starts as "Infectious")
+current_state = states.index("Symptomatic")
+
+# Function to transition to the next state based on probabilities
+def transition():
+    global current_state
+    next_state = random.choices(states, weights=transition_matrix[current_state])[0]
+    current_state = states.index(next_state)
+    return next_state
+
+# Simulate disease progression for an individual
+for _ in range(10):
+    next_state = transition()
+    print("Current state:", states[current_state])
+    print("Next state:", next_state)
+    print()
+
+
