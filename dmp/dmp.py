b747d67f30a1901027b6ad9f2c94e7b02d3a9fe1
Iason Mihalopoulos <imihalo1@jh.edu>
2023-10-14 21:57:24 -0400

    added user input and output to csv file

--- dmp/dmp.py
+++ dmp/dmp.py
@@ -1,4 +1,5 @@
-import random;
+import random
+import csv
 
 # Define states
 states = ["Symptomatic", "Infectious", "Hospitalized", "ICU", "Removed", "Recovered"]
@@ -14,8 +15,41 @@ transition_matrix = [
     [0.0,          0.0,        0.0,          0.0,     0.0,      1.0]    # Recovered
 ]
 
-# Initialize the initial state (e.g., a person starts as "Infectious")
-current_state = states.index("Symptomatic")
+# Set default values
+default_mean_time_interval = 5
+default_std_dev_time_interval = 2
+default_initial_state = "Symptomatic"
+
+# Get user input for parameters
+use_default_values = input("Use default values for parameters? (y/n): ").lower()
+
+if use_default_values == "n":
+    mean_time_interval = float(input("Enter the mean time interval: "))
+    std_dev_time_interval = float(input("Enter the standard deviation of time interval: "))
+    
+    while True:
+        initial_state = input("Enter the initial state (e.g., Symptomatic): ")
+        
+        # Check if the provided initial state is valid
+        if initial_state in states:
+            break
+        else:
+            print("Invalid initial state. Please choose from:", states)
+else:
+    mean_time_interval = default_mean_time_interval
+    std_dev_time_interval = default_std_dev_time_interval
+    initial_state = default_initial_state
+
+# Initialize the initial state based on user input
+current_state = states.index(initial_state)
+
+total_time_steps = 0
+
+# Create a list to store simulation data
+simulation_data = []
+
+# Set the desired number of iterations
+desired_iterations = 20
 
 # Function to transition to the next state based on probabilities
 def transition():
@@ -31,19 +65,17 @@ def sample_time_interval(mean, std_dev):
         if interval >= 0:
             return interval
 
-# Set parameters for time intervals (mean and standard deviation)
-mean_time_interval = 5  
-std_dev_time_interval = 2 
-
-total_time_steps = 0
-
-
-
 # Simulate disease progression for an individual
-while True:
+iterations = 0
+while iterations < desired_iterations:
     time_interval = sample_time_interval(mean_time_interval, std_dev_time_interval)
     total_time_steps += time_interval
-    print(f"Current state:", states[current_state], "|  Time step:", total_time_steps)
+    current_state_str = states[current_state]
+
+    # Append data to the list for data storage
+    simulation_data.append([current_state_str, total_time_steps])
+
+    print(f"Current state:", current_state_str, "| Time step:", total_time_steps)
 
     # Check if the current state is "Recovered" or "Removed" and stop the simulation
     if states[current_state] in ["Recovered", "Removed"]:
@@ -52,6 +84,12 @@ while True:
 
     next_state = transition()
     print()
+    
+    iterations += 1
 
-
-
+# Write the data to a CSV file
+csv_file_path = "dmp/simulation_results.csv"
+with open(csv_file_path, mode="w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(["State", "Time Step"])  # Write a header row
+    writer.writerows(simulation_data)
