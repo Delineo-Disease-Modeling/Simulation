fd19a20e54021fba941d159e377f0f371743b322
Iason Mihalopoulos <imihalo1@jh.edu>
2023-10-28 17:53:13 -0400

    added some values to only apply multipliers once

--- dmp/simulation_functions.py
+++ dmp/simulation_functions.py
@@ -36,6 +36,12 @@ def run_simulation(transition_matrix, mean_time_interval, std_dev_time_interval,
                 return interval
 
     iterations = 0
+    # Initialize flags for printing messages
+    age_multiplier_applied = False
+    ethnicity_multiplier_applied = False
+    group_quarters_multiplier_applied = False
+    length_of_stay_multiplier_applied = False
+
     while iterations < desired_iterations:
         time_interval = sample_time_interval(mean_time_interval, std_dev_time_interval)
         total_time_steps += time_interval
@@ -48,44 +54,44 @@ def run_simulation(transition_matrix, mean_time_interval, std_dev_time_interval,
 
         # Adjust transition probabilities based on age
         age_multiplier = 1.0
-        if age < 18:
-            # Decrease the risk of being removed for individuals under 18
+        if age < 18 and not age_multiplier_applied:
             age_multiplier = 0.8
             transition_matrix[states.index("Removed")][states.index("Removed")] *= age_multiplier
             print("Age multiplier applied")
+            age_multiplier_applied = True
 
         # Adjust transition probabilities based on ethnicity
         ethnicity_multiplier = 1.0
-        if ethnicity == "Asian":
-            # Increase the risk of symptomatic, infectious, and removed for Asian individuals
+        if ethnicity == "Asian" and not ethnicity_multiplier_applied:
             ethnicity_multiplier = 1.2
             transition_matrix[states.index("Symptomatic")][states.index("Symptomatic")] *= ethnicity_multiplier
             transition_matrix[states.index("Infectious")][states.index("Infectious")] *= ethnicity_multiplier
             transition_matrix[states.index("Removed")][states.index("Removed")] *= ethnicity_multiplier
             print("Ethnicity multiplier applied (Asian)")
-        elif ethnicity == "White":
-            # Increase the risk of symptomatic and infectious for White individuals
+            ethnicity_multiplier_applied = True
+        elif ethnicity == "White" and not ethnicity_multiplier_applied:
             ethnicity_multiplier = 1.1
             transition_matrix[states.index("Symptomatic")][states.index("Symptomatic")] *= ethnicity_multiplier
             print("Ethnicity multiplier applied (White)")
+            ethnicity_multiplier_applied = True
 
         # Adjust transition probabilities based on group_quarters
         group_quarters_multiplier = 1.0
-        if group_quarters:
-            # Increase the risk of symptomatic and infectious for individuals in group quarters
+        if group_quarters and not group_quarters_multiplier_applied:
             group_quarters_multiplier = 1.1
             transition_matrix[states.index("Symptomatic")][states.index("Symptomatic")] *= group_quarters_multiplier
             transition_matrix[states.index("Infectious")][states.index("Infectious")] *= group_quarters_multiplier
             print("Group Quarters multiplier applied")
+            group_quarters_multiplier_applied = True
 
         # Adjust transition probabilities based on length_of_stay
         length_of_stay_multiplier = 1.0
-        if length_of_stay > 1:
-            # Decrease the risk of symptomatic and infectious for individuals with a length of stay greater than 1 year
+        if length_of_stay > 1 and not length_of_stay_multiplier_applied:
             length_of_stay_multiplier = 0.9
             transition_matrix[states.index("Symptomatic")][states.index("Symptomatic")] *= length_of_stay_multiplier
             transition_matrix[states.index("Infectious")][states.index("Infectious")] *= length_of_stay_multiplier
             print("Length of Stay multiplier applied")
+            length_of_stay_multiplier_applied = True
 
         # Use the adjusted transition matrix for the next state transition
         next_state = transition()
