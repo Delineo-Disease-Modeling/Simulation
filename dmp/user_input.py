be491926e5d9bf645a62cf2654e6337e36552d47
Neil Patel <neilpatel369@gmail.com>
2023-11-08 14:08:52 -0500

    Working with new input file

--- dmp/user_input.py
+++ dmp/user_input.py
@@ -3,106 +3,105 @@ import csv
 import os
 from simulation_functions import run_simulation, states, default_mean_time_interval, default_std_dev_time_interval, default_initial_state, transition_matrix
 
-def read_parameters_from_csv(csv_filename):
-    try:
-        with open(csv_filename, mode="r", newline="") as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                return row  # Return the first row found in the CSV
-        print("CSV file is empty or does not contain valid parameters.")
-        return None
-    except FileNotFoundError:
-        print("CSV file not found.")
-        return None
+# def read_parameters_from_csv(csv_filename):
+#     try:
+#         with open(csv_filename, mode="r", newline="") as file:
+#             reader = csv.reader(file)
+#             return list(reader)  # Return all rows in the CSV as a list of lists
+#     except FileNotFoundError:
+#         print("CSV file not found.")
+#         return None
 
-def get_output_csv_path(csv_filename):
-    base_filename = os.path.splitext(csv_filename)[0]
-    return f"{base_filename}_results.csv"
 
-def get_user_input():
-    use_default_values = input("Use default values for parameters? (y/n): ").lower()
 
-    if use_default_values == "n":
-        parameters = read_parameters_from_csv(args.csv_file)
-        if parameters:
-            mean_time_interval = float(parameters.get("Mean Time Interval", default_mean_time_interval))
-            std_dev_time_interval = float(parameters.get("Standard Deviation", default_std_dev_time_interval))
-            initial_state = parameters.get("Initial State", default_initial_state)
-            desired_iterations = int(parameters.get("Desired Iterations", 20))
+# def get_output_csv_path(csv_filename):
+#     base_filename = os.path.splitext(csv_filename)[0]
+#     return f"{base_filename}_results.csv"
 
-            # Age input validation
-            age = int(parameters.get("Age", 0))
-            if age < 0 or age > 120:
-                print("Age should be between 0 and 120. Using default value.")
-                age = 0
+# def get_user_input(args):
+#     if not args.csv_file:
+#         print("Please provide a CSV file with simulation parameters using the --csv_file argument.")
+#         return None
 
-            ethnicity = parameters.get("Ethnicity", "")
-            group_quarters = parameters.get("Group Quarters", "no").lower() == "yes"
+#     use_default_values = input("Use default values for parameters? (y/n): ").lower()
 
-            # Length of Stay input validation
-            length_of_stay = int(parameters.get("Length of Stay", 0))
-            if length_of_stay < 0:
-                print("Length of Stay should be a positive integer. Using default value.")
-                length_of_stay = 0
+#     if use_default_values == "n":
+#         parameters = read_parameters_from_csv(args.csv_file)
+#         if not parameters:
+#             return None
 
-            # Number of People Under 5 input validation
-            num_under_5 = int(parameters.get("Number of People Under 5", 0))
-            if num_under_5 < 0:
-                print("Number of People Under 5 should be a non-negative integer. Using default value.")
-                num_under_5 = 0
+#         simulation_data = []
+#         for row in parameters:
+#             if len(row) >= 3:  # Ensure the row has enough elements for unpacking
+#                 sex, age, comorbidity = row[0].split(", ")[:3]  # Limit to 3 elements
+#                 transition_matrix_2 = [list(map(float, entry.split(", "))) for entry in row[1:]]
+#                 result = run_simulation(transition_matrix_2, default_mean_time_interval, default_std_dev_time_interval, default_initial_state, 20)
+#                 simulation_data.append(result)
+#             else:
+#                 print("Insufficient data in the row:", row)
 
-            mobile_home = parameters.get("Mobile Home", "no").lower() == "yes"
-            origin = parameters.get("Origin", "")
-            print("Loaded parameters from CSV file.")
-        else:
-            # If the CSV file is not found or doesn't contain valid parameters, use defaults.
-            mean_time_interval = default_mean_time_interval
-            std_dev_time_interval = default_std_dev_time_interval
-            initial_state = default_initial_state
-            desired_iterations = 20
-            age = 0
-            ethnicity = ""
-            group_quarters = False
-            length_of_stay = 0
-            num_under_5 = 0
-            mobile_home = False
-            origin = ""
-            print("Using default parameters.")
-    else:
-        # Use default values
-        mean_time_interval = default_mean_time_interval
-        std_dev_time_interval = default_std_dev_time_interval
-        initial_state = default_initial_state
-        desired_iterations = 20
-        age = 0
-        ethnicity = ""
-        group_quarters = False
-        length_of_stay = 0
-        num_under_5 = 0
-        mobile_home = False
-        origin = ""
-        print("Using default parameters.")
+#     else:
+#         print("Using default parameters.")
+#         simulation_data = run_simulation(transition_matrix, default_mean_time_interval, default_std_dev_time_interval, default_initial_state, 20)
 
-    output_csv_path = get_output_csv_path(args.csv_file)
-    simulation_data = run_simulation(transition_matrix, mean_time_interval, std_dev_time_interval, initial_state, desired_iterations, age, ethnicity, group_quarters, length_of_stay, num_under_5, mobile_home, origin)
+#     output_csv_path = get_output_csv_path(args.csv_file)
+#     with open(output_csv_path, mode="w", newline="") as file:
+#         writer = csv.writer(file)
+#         writer.writerow(["State", "Time Step"])
+#         writer.writerows(simulation_data)
 
-    with open(output_csv_path, mode="w", newline="") as file:
-        writer = csv.writer(file)
-        writer.writerow(["State", "Time Step"])
-        writer.writerows(simulation_data)
+#     return simulation_data
 
-    return simulation_data
+# if __name__ == "__main__":
+#     parser = argparse.ArgumentParser(description="Disease Simulation")
+#     parser.add_argument("--csv_file", type=str, help="CSV file containing simulation parameters")
+#     args = parser.parse_args()
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Disease Simulation")
-    parser.add_argument("--csv_file", type=str, help="CSV file containing simulation parameters")
-    args = parser.parse_args()
+#     simulation_data = get_user_input(args)  # Pass 'args' as an argument
 
-    if not args.csv_file:
-        print("Please provide a CSV file with simulation parameters using the --csv_file argument.")
-    else:
-        simulation_data = get_user_input()
-        print("Simulation completed. Results:")
-        for state, time_step in simulation_data:
-            print(f"State: {state}, Time Step: {time_step}")
-        print(f"Results saved to {get_output_csv_path(args.csv_file)}")
+#     if not args.csv_file:
+#         print("Please provide a CSV file with simulation parameters using the --csv_file argument.")
+#     else:
+#         simulation_data = get_user_input()
+#         print("Simulation completed. Results:")
+#         for state, time_step in simulation_data:
+#             print(f"State: {state}, Time Step: {time_step}")
+#         print(f"Results saved to {get_output_csv_path(args.csv_file)}")
+
+import pandas as pd
+import numpy as np
+
+# Read the input file
+with open('simulation_parameters.csv', 'r') as file:
+    sample_data = file.readline().strip()  # Read the first line (strip to remove newline character)
+
+# Split the sample data by comma
+split_data = sample_data.split(', ')
+
+# Extract sex, age, and comorbidity
+sex = split_data[0]
+age = int(split_data[1])
+comorbidity = split_data[2]
+
+# Extract the transition matrix values as a 6x7 array
+transition_values = np.array([float(value) for value in split_data[3:]])
+
+# Reshape the transition values into a 6x7 matrix
+transition_matrix = transition_values.reshape(6, 7)
+
+# Create a DataFrame combining sex, age, and comorbidity with the transition matrix
+data = pd.DataFrame(transition_matrix)
+
+simulation_data = run_simulation(transition_matrix, default_mean_time_interval, default_std_dev_time_interval, default_initial_state, 20)
+output_file = "simulation_parameters_results.csv"
+with open(output_file, "w", newline='') as file:
+      writer = csv.writer(file)
+      writer.writerow(["State", "Time Step"])
+      writer.writerows(simulation_data)
+
+# Display the extracted information and transition matrix
+print("Sex:", sex)
+print("Age:", age)
+print("Comorbidity:", comorbidity)
+print("\nTransition Matrix:")
+print(data)
