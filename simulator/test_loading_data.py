c90b173108db90853fab34f51b42aee545fc64fc
NavyaMehrotra1 <nmehrot2@jh.edu>
2025-04-06 17:11:27 -0400

    wrote method to work with database

--- simulator/test_loading_data.py
+++ simulator/test_loading_data.py
@@ -1,11 +1,5 @@
-from data_interface import load_people, load_places, load_sample_data
+from data_interface import *
 import json
 
-pap = load_sample_data()
-people = pap['people']
-places = pap['places']
-print("All data", pap)
-print("---------------")
-print("People data", people)
-print("---------------")
-print("Places data", places)
+data = load_movement_pap_data("0")
+print(data)
