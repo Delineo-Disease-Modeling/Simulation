a9d42c3214c053a4d1aa4f8e896d2509afb05872
NavyaMehrotra1 <nmehrot2@jh.edu>
2025-04-07 12:42:54 -0400

    calling data library for pap_data and movement patterns

--- simulator/test_loading_data.py
+++ simulator/test_loading_data.py
@@ -1,5 +1,10 @@
 from data_interface import *
 import json
 
-data = load_movement_pap_data("0")
+data = load_movement_pap_data()
 print(data)
+
+movement_patterns = data['movement_patterns']
+pap_data = data['papdata']
+print(movement_patterns)
+print(pap_data)
