024db03f3938de29cb60fdf0a13146f07228b9f9
NavyaMehrotra1 <nmehrot2@jh.edu>
2025-04-13 16:43:45 -0400

    changes to load data from central database

--- simulator/test_loading_data.py
+++ simulator/test_loading_data.py
@@ -2,6 +2,35 @@ from data_interface import *
 import json
 
 data = load_movement_pap_data()
-print(data)
+patterns = data.get("data", {}).get("patterns", {})
+papdata = data.get("data", {}).get("papdata", {})
+people_data = papdata.get("people", {})
+places_data = papdata.get("places", {})
+homes_data = papdata.get("homes", {})
+
+print("Movement Patterns: ")
+print(patterns)
+print("Patterns printed successfully")
+print("-------------------------------")
+
+print("People and places data")
+print(papdata)
+print("pap data printed successfully")
+print("-------------------------------")
+
+print("People data")
+print(people_data)
+print("People data printed successfully")   
+print("-------------------------------")
+
+print("Homes data")
+print(homes_data)
+print("-------------------------------")
+
+
+print("Places data")
+print(places_data)
+print("-------------------------------")
+
 
 
