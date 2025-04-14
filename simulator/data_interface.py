da7ce135f521e0466aae11cd7140e8477164bcb5
NavyaMehrotra1 <nmehrot2@jh.edu>
2025-04-14 16:17:26 -0400

    integration (using sample data)

--- simulator/data_interface.py
+++ simulator/data_interface.py
@@ -3,7 +3,7 @@ import requests
 BASE_URL = "https://db.delineo.me/"
 
 def load_movement_pap_data(cz_id=1): 
-    url = "https://db.delineo.me/patterns/1"
+    url = "https://db.delineo.me/patterns/2"
     
     try:
         response = requests.get(url)
