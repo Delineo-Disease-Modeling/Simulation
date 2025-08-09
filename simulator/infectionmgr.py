680a7a1b680c2089a420bc665293c046fb9aa4db
NavyaMehrotra1 <nmehrot2@jh.edu>
2025-08-09 18:01:13 -0400

    multiple tests

--- simulator/infectionmgr.py
+++ simulator/infectionmgr.py
@@ -1,5 +1,5 @@
 from .pap import InfectionState, InfectionTimeline, VaccinationState
-from .infection_models.v2_wells_riley import CAT
+from .infection_models.v4_wells_riley import CAT
 from .config import DMP_API, INFECTION_MODEL
 import pandas as pd
 from io import StringIO
